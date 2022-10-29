b609c76dcbfc011de908b3f118a84b6c6b7b7993
Mayur Andulkar <man@tes-h2.com>
2022-10-29 22:55:27 +0200

    Updated

--- optimization/optimize_working.py
+++ optimization/optimize_working.py
@@ -83,86 +83,6 @@ class Process():
         self.p.m.process_c_credits = pe.Expression(rule=get_process_c_credits)
         print('Defined C credits')
 
-def init():
-    print('Inside init')
-
-def request_parser(request, windowSize):
-    #extracting process data 
-    process_dat=pd.DataFrame.from_records(request['processes'])
-    
-    url = 'https://carbon-aware-api.azurewebsites.net/emissions/forecasts/current' 
-    loc = request['location']
-    startTime = str(request['startTime']) 
-    endTime = str(request['endTime'])
-    final_url= url + '?location=' + request['location'] + '&dataStartAt=' +startTime + '&dataEndAt='+endTime + '&windowSize='+windowSize
-    return final_url, process_dat
-
-def nooptim(data, process_data,request):
-    carbon_dat=json_normalize(json.loads(data))
-    nooptim_proc={}
-    return nooptim_proc
-
-def webservice(url):
-    response =requests.get(url)
-    fjson=json.loads(response.text)
-    data_file = json.dumps(fjson[0])
-    result=json.loads(data_file)['forecastData']
-    return str.encode(json.dumps(result))
-
-def restructure_input_req(carbon_api_data, process_data, windowSize):
-    c_df = pd.read_json(carbon_api_data)
-    indices = c_df.index.to_list()
-    start_index = indices[0]
-    end_index = indices[-1]
-    input = dict()
-    for proc in process_data['processes']:
-        input[proc['name']] = {
-        "start_window": start_index,
-        "end_window": end_index,
-        "duration": int(proc['duration']/windowSize),
-        "dependencies": proc['dependencies']
-        }
-    return input
-
-def shortest_processing_time(carbon_api_data, process_data, windowSize):
-    processes = restructure_input_req(carbon_api_data, process_data, windowSize)
-    SCHEDULE = dict()
-    unfinished_processes = set(processes.keys())
-    start = 0
-    while len(unfinished_processes) > 0:
-        start = max(start, min(processes[process]['start_window'] for process in unfinished_processes))
-        spt = {process:processes[process]['duration'] for process in unfinished_processes if processes[process]['start_window'] <= start}
-        process = min(spt, key=spt.get)
-        finish = start + processes[process]['duration']
-        unfinished_processes.remove(process)
-        SCHEDULE[process] = {'start': start, 'finish': finish}
-        start = finish
-
-    print(SCHEDULE)
-    return None
-
-def run(request, windowSize=5):
-    """
-    This function is called for every invocation of the endpoint to perform the actual scoring/prediction.
-    In the example we extract the data from the json input and call the scikit-learn model's predict()
-    method and return the result back
-    """
-    print("Request received")
-    print(request)
-    print('creating url for extracting data')
-    url, process_df= request_parser(request, str(windowSize))
-    print('Getting data based on the request')
-    data = webservice(url)
-    print('calling NO Optimisation')
-    nooptim_proc = nooptim(data, process_df, request)
-
-    print('calling Shortest Processing Time')
-    shortest_processing_time(data, request, windowSize)
-
-    # print('calling Optimisation')
-    # opti_model(data, request)
-
-    return data
 
 class ModelData(ABC):
     """
@@ -278,6 +198,145 @@ def opti_model(carbon_api_data, process_data):
     overall_process.solve()
 
     return round(pe.value(overall_process.m.process_opti_start),3)
+
+
+def opt_schedule(processes, carbon_api_data):
+    # create model
+    m = pe.ConcreteModel()
+
+    # Get carbon data in API model
+    c_df = pd.read_json(carbon_api_data)
+    hr_mins = c_df.index.to_list()
+    m.hr_mins = pe.Set(initialize=hr_mins)
+    m.carbon_value = c_df['value'].to_dict()
+
+    # index set to simplify notation
+    m.processes = pe.Set(initialize=processes.keys())
+
+    # decision variables
+    m.start = pe.Var(m.processes, domain=pe.NonNegativeIntegers)
+    m.finish = pe.Var(m.processes, domain=pe.NonNegativeIntegers)
+    
+
+    def get_total_c_credits(model):
+        carbon_credits = 0
+        for j in model.processes:
+            carbon_credits += model.start[j]# *model.carbon_value[model.start[j]]
+        return carbon_credits
+    m.total_c_credits = pe.Expression(rule=get_total_c_credits)
+    
+    # objective function
+    m.OBJ = pe.Objective(expr = m.total_c_credits, sense = pe.maximize)
+    
+    # constraints
+    m.c = pe.ConstraintList()
+    for j in m.processes:
+        # Total process duration should be preserved
+        m.c.add(m.finish[j] == m.start[j] + processes[j]['duration'])
+
+        m.c.add(m.start[j] <= processes[j]['end_window']-processes[j]['duration'])
+
+        # Process can start only based on start window
+        m.c.add(m.start[j] >= processes[j]['start_window'])
+
+    pe.SolverFactory('cbc').solve(m)
+    
+    schedule = {}
+    for j in m.processes:
+        schedule[j] = {'start': m.start[j](), 'finish': m.start[j]() + processes[j]['duration']}
+        
+    return schedule
+
+def init():
+    print('Inside init')
+
+def request_parser(request, windowSize):
+    #extracting process data 
+    process_dat=pd.DataFrame.from_records(request['processes'])
+    
+    url = 'https://carbon-aware-api.azurewebsites.net/emissions/forecasts/current' 
+    loc = request['location']
+    startTime = str(request['startTime']) 
+    endTime = str(request['endTime'])
+    final_url= url + '?location=' + request['location'] + '&dataStartAt=' +startTime + '&dataEndAt='+endTime + '&windowSize='+windowSize
+    return final_url, process_dat
+
+def nooptim(data, process_data,request):
+    carbon_dat=json_normalize(json.loads(data))
+    nooptim_proc={}
+    return nooptim_proc
+
+def webservice(url):
+    response =requests.get(url)
+    print(response)
+    if response.ok:
+        fjson=json.loads(response.text)
+        data_file = json.dumps(fjson[0])
+        result=json.loads(data_file)['forecastData']
+        return str.encode(json.dumps(result))
+    else:
+        print('Did not get data from Carbon API')
+        return None
+
+def restructure_input_req(carbon_api_data, process_data, windowSize):
+    c_df = pd.read_json(carbon_api_data)
+    indices = c_df.index.to_list()
+    start_index = indices[0]
+    end_index = indices[-1]
+    input = dict()
+    for proc in process_data['processes']:
+        input[proc['name']] = {
+        "start_window": start_index,
+        "end_window": end_index,
+        "duration": int(proc['duration']/windowSize),
+        "dependencies": proc['dependencies']
+        }
+    return input
+
+def shortest_processing_time(processes):
+    schedule = dict()
+    unfinished_processes = set(processes.keys())
+    start = 0
+    while len(unfinished_processes) > 0:
+        start = max(start, min(processes[process]['start_window'] for process in unfinished_processes))
+        spt = {process:processes[process]['duration'] for process in unfinished_processes if processes[process]['start_window'] <= start}
+        process = min(spt, key=spt.get)
+        finish = start + processes[process]['duration']
+        unfinished_processes.remove(process)
+        schedule[process] = {'start': start, 'finish': finish}
+        start = finish
+
+    print(schedule)
+    return None
+
+def run(request, windowSize=5):
+    """
+    This function is called for every invocation of the endpoint to perform the actual scoring/prediction.
+    In the example we extract the data from the json input and call the scikit-learn model's predict()
+    method and return the result back
+    """
+    print("Request received")
+    print(request)
+    print('creating url for extracting data')
+    url, process_df= request_parser(request, str(windowSize))
+    print('Getting data based on the request')
+    carbon_api_data = webservice(url)
+
+    if carbon_api_data:
+        print('calling NO Optimisation')
+        nooptim_proc = nooptim(carbon_api_data, process_df, request)
+
+        processes = restructure_input_req(carbon_api_data, request, windowSize)
+
+        print('calling Shortest Processing Time')
+        # shortest_processing_time(processes)
+
+        print('calling Optimisation')
+        schedule = opt_schedule(processes, carbon_api_data)
+        print(schedule)
+        # opti_model(data, request)
+
+    return carbon_api_data
 
 
 def main():
